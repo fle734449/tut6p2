494c6036283e7387a05f187d9eeb24fe4b778049
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-03-02 21:39:19 -0600

    initial

--- src/tut6p2/MinWebTest.java
+++ src/tut6p2/MinWebTest.java
@@ -10,11 +10,11 @@ import org.junit.jupiter.api.Test;
 public class MinWebTest {
 	@Test public void t0() {
 		// execute the test <x = 0, y = 0, z = 0, submitButton = click> and check the output message is correct
-		System.setProperty("webdriver.gecko.driver","TODO: /path/to/geckodriver");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Frank\\Documents\\GitHub\\tut6p2\\geckodriver.exe");
 		WebDriver wd = new FirefoxDriver(); // launch the browser
 		// edit the next line to enter the location of "min.html" on your file system
-		wd.get("file:///C:/Users/.../min.html");
-		WebElement we = wd.findElement(By.id(???));
+		wd.get("C:\\Users\\Frank\\Documents\\GitHub\\tut6p2\\min.html");
+		WebElement we = wd.findElement(By.id(x));
 		we.sendKeys(???); // enter a representative for x
 		we = wd.findElement(By.id(???));
 		we.sendKeys(???); // enter a representative for y
